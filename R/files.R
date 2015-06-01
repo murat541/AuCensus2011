7e0df50a1696f9224cbe77c46cc5dcd4dbb79dc6
mike <mikefc@planckenergy.com>
2015-06-01 21:09:06 +1000

    construct the full path to an abs file given profile, table, level

---
+++ R/files.R
@@ -0,0 +1,34 @@
+
+#=============================================================================
+# Code for general file pathnames, manipulation etc
+#=============================================================================
+
+#' Path to ABS DataPack CSV file
+#'
+#' \code{create_abs_filename} creates the path to a given ABS datapack file.
+#'
+#' MFC TODO: Describe exactly the file layout.
+#' For now, root_dir = place where DataPack zip files were uncompressed to.
+#'
+#' @param profile The short code for the profile. e.g. 'BCP' for Basic Community Profile.
+#' @param table Table number including and suffix. e.g. 'B01', 'B08A'
+#' @param level The level of the statistcal area: [AUS, CED, GCCSA, IARE, ILOC, IREG, LGA, POA, RA, SA1, SA2, SA3, SA4, SED, SLA, SOS, SOSR, SSC, STE, SUA, UCL]
+#' @return Returns the full path to the CSV file containing the nominated data.
+#' @examples
+#' create_abs_filename('BCP', 'B46', 'SSA')
+#'
+create_abs_filename <- function(profile, table, level) {
+    root_dir <- file.path(Sys.getenv('HOME'), "projectsdata/ABS2011/DataPacks/")
+    if (level == 'AUS') {
+        abs_filename <- paste0(root_dir,
+                               "2011_", profile, "_ALL_for_AUST_long-header/",
+                               "2011 Census ", profile, " All Geographies for AUST/",
+                               "/AUST/2011Census_", table, "_AUST_long.csv")
+    } else {
+        abs_filename <- paste0(root_dir,
+                               "2011_", profile, "_ALL_for_AUST_long-header/",
+                               "2011 Census ", profile, " All Geographies for AUST/",
+                               level, "/AUST/2011Census_", table, "_AUST_", level, "_long.csv")
+    }
+    abs_filename
+}
