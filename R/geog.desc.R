5220c7cb737ac9d59e2c8fd1931c3d09d086fe0e
mike <mikefc@planckenergy.com>
2015-06-01 20:54:07 +1000

    added geographical descriptors data

---
+++ R/geog.desc.R
@@ -0,0 +1,15 @@
+#' ABS Australian Census 2011: Geographical Descriptors
+#'
+#' ABS Australian Census 2011 Geographical Descriptors
+#'
+#' Metadata concerning the description of the different statistical regions for the 2011 Australian Census Data.
+#'
+#' @source Based on Australian Bureau of Statistics data \url{http://www.abs.gov.au/websitedbs/censushome.nsf/home/datapacksdetails/$file/2011Census_geog_desc_1st_and_2nd_release.csv}
+#' @format Data frame with columns
+#' \describe{
+#'  \item{level}{AUS, SSC, STE etc}
+#'  \item{region_id}{ID for region within a statistical area}
+#'  \item{label}{Full text label}
+#'  \item{area}{Area of region in square kms}
+#' }
+"geog.desc"
