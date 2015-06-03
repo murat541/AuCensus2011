754c7df27f0cf28f27dc693989e769ee7be1d8e3
mike <mikefc@planckenergy.com>
2015-06-03 16:57:43 +1000

    refactored variable splitting. 'config' based splitting

--- R/varsplitting.R
+++ R/varsplitting.R
@@ -8,27 +8,50 @@
 # - e.g.  origin = Bosnia_and_Herzegovina   year_range = <1941
 #=============================================================================
 
-#' Variable Split by Gender
-#'
-#' Split the gender from a measure variable into it's own column
-#'
-#' Long description here.
-#'
-#' @param colname vector of column names to split
-#'
-split_bygender <- function(longdata) {
-    longdata %>% mutate(colname = as.character(colname)) %>%
-        mutate(stat   = str_match(colname, pattern="(.*?)_(Males|Females|Persons)$")[,2],
-               gender = str_match(colname, pattern="(.*?)_(Males|Females|Persons)$")[,3]) %>%
-        select(region_id, colname, stat, gender, count)
+
+#-----------------------------------------------------------------------------
+# split a vector of column names given a table configuration.
+# config$patterns : regexes to match and split the names
+# config$stats    : the names of the new split variables
+#                   number of stats must match the number of captured groups
+#                   in the set of patterns.
+#-----------------------------------------------------------------------------
+split_column_names <- function(column_names, config) {
+    column_names <- as.character(unique(column_names))
+    labels <- dplyr::data_frame(colname=column_names)
+    # initialise blank columns for dumping the split variables
+    for (stat in config$stats) {
+        labels[[stat]] <- ''
+    }
+
+    # match the patterns and split the column names
+    for (pattern in config$patterns) {
+        # Find the ones which match the pattern
+        ii    <- str_detect(column_names, pattern)
+        # Split on those that match the pattern
+        match <- str_match(column_names[ii], pattern)
+        # assign the vars into the labels df
+        mcount <- 2
+        for (stat in config$stats) {
+            labels[[stat]][ii] <- match[,mcount]
+            mcount <- mcount + 1
+        }
+    }
+
+    if (!is.null(config[['extra_manipulation']])) {
+        labels <- labels %>% (config[['extra_manipulation']])
+    }
+
+    labels
 }
 
-#' operates on a single string at a time.
-split_b01_columns <- function(column_names) {
-    column_names <- as.character(column_names)
-    labels <- dplyr::data_frame(colname=column_names, stat1="", stat2="", gender="")
+#-----------------------------------------------------------------------------
+# Configs
+#-----------------------------------------------------------------------------
 
-    patterns <- c(
+b01config <- list(
+    table    = 'B01',
+    patterns = c(
         '^(Total_Persons|Australian_citizen)()_(Males|Females|Persons)$',
         '^(Age_group)s_(.*)_years.*_(Males|Females|Persons)$',
         '^(Counted_on_Census_Night)_(.*)_(Males|Females|Persons)$',
@@ -38,23 +61,13 @@ split_b01_columns <- function(column_names) {
         '^(Age_of_Persons_attending_an_educational_institution)_(.*)_years.*_(Males|Females|Persons)',
         '^(Highest_year_of_school_completed)_(.*?)_(Males|Females|Persons)$',
         '^(Count_of_Persons)_in_(.*)_(Males|Females|Persons)$'
-    )
+    ),
+    stats = c('stat1', 'stat2', 'gender')
+)
 
-    for (pattern in patterns) {
-        ii <- str_detect(column_names, pattern)
-        match <- str_match(column_names[ii], pattern)
-        labels$stat1[ii]  <- match[,2]
-        labels$stat2[ii]  <- match[,3]
-        labels$gender[ii] <- match[,4]
-    }
-    labels
-}
-
-split_b03_columns <- function(column_names) {
-    column_names <- as.character(column_names)
-    labels <- dplyr::data_frame(colname=column_names, stat1="", stat2="")
-
-    patterns <- c(
+b03config <- list(
+    table    = 'B03',
+    patterns = c(
         '^(Counted_at_home_on_Census_Night)_Age_(.*)_years',
         '^(Counted_at_home_on_Census_Night)_(Total)$',
 
@@ -71,171 +84,84 @@ split_b03_columns <- function(column_names) {
         '^Visitor_from_(Total_visitors)_(Total)$',
         '^(Total)_Age_(.*)_years',
         '^(Total)_(Total)$'
-    )
+    ),
+    stats = c('stat1', 'stat2')
+)
 
-    for (pattern in patterns) {
-        ii <- str_detect(column_names, pattern)
-        match <- str_match(column_names[ii], pattern)
-        labels$stat1[ii]  <- match[,2]
-        labels$stat2[ii]  <- match[,3]
-    }
-    labels
-}
-
-split_b04_columns <- function(column_names) {
-    column_names <- as.character(unique(column_names))
-    labels <- dplyr::data_frame(colname=column_names, stat1="", stat2="", gender="")
-
-    patterns <- c(
+b04config <- list(
+    table    = 'B04',
+    patterns = c(
         '^Age_years_(.*)_(Males|Females|Persons)$',
         '^(Total)_(Males|Females|Persons)$'
-    )
+    ),
+    stats = c('stat1', 'gender')
+)
 
-    for (pattern in patterns) {
-        ii <- str_detect(column_names, pattern)
-        match <- str_match(column_names[ii], pattern)
-        labels$stat1[ii]   <- match[,2]
-        labels$gender[ii]  <- match[,3]
-    }
-    labels
-}
+b05config <- list(
+    table    = 'B05',
+    patterns = c(
+        '^(Males|Females|Persons)_(.*)_(Total|Widowed|Divorced|Never_Married|Married|Separated)$',
+        '^(Males|Females|Persons)_(.*)_(Never_Married)$'
+    ),
+    stats = c('stat1', 'stat2', 'gender')
+)
 
-
-split_b05_columns <- function(column_names) {
-    column_names <- as.character(unique(column_names))
-    labels <- dplyr::data_frame(colname=column_names, stat1="", stat2="", gender="")
-
-    patterns <- c(
-        '^(Males|Females|Persons)_(.*)_(Total|Widowed|Married|Divorced|Never_Married|Separated)$'
-    )
-
-    for (pattern in patterns) {
-        ii <- str_detect(column_names, pattern)
-        match <- str_match(column_names[ii], pattern)
-        labels$stat1[ii]   <- match[,3]
-        labels$stat2[ii]   <- match[,4]
-        labels$gender[ii]  <- match[,2]
-    }
-    labels
-}
-
-split_b06_columns <- function(column_names) {
-    column_names <- as.character(unique(column_names))
-    labels <- dplyr::data_frame(colname=column_names, stat1="", stat2="", gender="")
-
-    patterns <- c(
+b06config <- list(
+    table    = 'B06',
+    patterns = c(
         '^(Males|Females|Persons)_(.*)_(Total|Not_married|Married_in_a_registered_marriage|Married_in_a_de_facto_marriage)$'
-    )
+    ),
+    stats = c('gender', 'stat1', 'stat2')
+)
 
-    for (pattern in patterns) {
-        ii <- str_detect(column_names, pattern)
-        match <- str_match(column_names[ii], pattern)
-        labels$stat1[ii]   <- match[,3]
-        labels$stat2[ii]   <- match[,4]
-        labels$gender[ii]  <- match[,2]
-    }
-    labels
-}
-
-split_b07_columns <- function(column_names) {
-    column_names <- as.character(unique(column_names))
-    labels <- dplyr::data_frame(colname=column_names, stat1="", stat2="", gender="")
-
-    patterns <- c(
+b07config <- list(
+    table    = 'B07',
+    patterns = c(
         '^(.*)_years_(.*)_(Males|Females|Persons)$',
         '^(.*)_years_and_over_(.*)_(Males|Females|Persons)$',
         '^(Total)_(.*)_(Males|Females|Persons)$'
-    )
+    ),
+    stats = c('stat1', 'stat2', 'gender')
+)
 
-    for (pattern in patterns) {
-        ii <- str_detect(column_names, pattern)
-        match <- str_match(column_names[ii], pattern)
-        labels$stat1[ii]   <- match[,2]
-        labels$stat2[ii]   <- match[,3]
-        labels$gender[ii]  <- match[,4]
-    }
-    labels
-}
-
-split_b08_columns <- function(column_names) {
-    column_names <- as.character(unique(column_names))
-    labels <- dplyr::data_frame(colname=column_names, stat1="", stat2="", stat3="")
-
-    patterns <- c(
+b08config <- list(
+    table    = 'B08',
+    patterns = c(
         '^(.*)_(Mother_only|Father_only|Both_parents)_(born_in_Australia|born_overseas)$',
         '^(.*)()_(Birthplace_not_stated|Total_Responses)$'
-    )
+    ),
+    stats = c('stat1', 'stat2', 'stat3')
+)
 
-    for (pattern in patterns) {
-        ii <- str_detect(column_names, pattern)
-        match <- str_match(column_names[ii], pattern)
-        labels$stat1[ii]  <- match[,2]
-        labels$stat2[ii]  <- match[,3]
-        labels$stat3[ii]  <- match[,4]
-    }
-    labels
-}
-
-split_b09_columns <- function(column_names) {
-    column_names <- as.character(unique(column_names))
-    labels <- dplyr::data_frame(colname=column_names, stat1="", gender="")
-
-    patterns <- c(
+b09config <- list(
+    table    = 'B09',
+    patterns = c(
         '^(.*)_(Males|Females|Persons|Person|Total)$'
-    )
+    ),
+    stats = c('stat1', 'gender'),
+    extra_manipulation = . %>% mutate(gender = ifelse(gender %in% c('Total', 'Person'), 'Persons', gender))
+)
 
-    for (pattern in patterns) {
-        ii <- str_detect(column_names, pattern)
-        match <- str_match(column_names[ii], pattern)
-        labels$stat1[ii]  <- match[,2]
-        labels$gender[ii] <- match[,3]
-    }
-
-    # ABS FIX: some column names badly indicated for gender
-    labels$gender[labels$gender %in% c('Total', 'Person')] <- 'Persons'
-
-    labels
-}
-
-split_b10_columns <- function(column_names) {
-    column_names <- as.character(unique(column_names))
-    labels <- dplyr::data_frame(colname=column_names, stat1="", stat2="")
-
-    patterns <- c(
+b10config <- list(
+    table    = 'B10',
+    patterns = c(
         '^(.*)_Year_of_arrival_(.*)$',
         '^(.*)_(Total)$'
-    )
+    ),
+    stats = c('stat1', 'stat2')
+)
 
-    for (pattern in patterns) {
-        ii <- str_detect(column_names, pattern)
-        match <- str_match(column_names[ii], pattern)
-        labels$stat1[ii] <- match[,2]
-        labels$stat2[ii] <- match[,3]
-    }
-
-    labels
-}
-
-split_b46_columns <- function(column_names) {
-    column_names <- as.character(unique(column_names))
-    labels <- dplyr::data_frame(colname=column_names, stat1="", stat2="", stat3="")
-
-    patterns <- c(
+b46config <- list(
+    table    = 'B46',
+    patterns = c(
         '^(.*)()_(Males|Females|Persons)$',
         '^(One_method|Two_methods|Three_methods)_(.*)_(Males|Females|Persons)$'
-    )
+    ),
+    stats = c('stat1', 'stat2', 'gender')
+)
 
-    for (pattern in patterns) {
-        ii <- str_detect(column_names, pattern)
-        match <- str_match(column_names[ii], pattern)
-        labels$stat1[ii] <- match[,2]
-        labels$stat2[ii] <- match[,3]
-        labels$stat3[ii] <- match[,4]
-    }
-
-    labels
+if (FALSE) {
+    df <- read_abs('BCP', 'B10', 'AUS', long=TRUE); column_names <- df$colname
+    df <- split_column_names(df$colname, b10config)
+    df
 }
-
-
-df <- read_abs('BCP', 'B46', 'AUS', long=TRUE); column_names <- df$colname
-
