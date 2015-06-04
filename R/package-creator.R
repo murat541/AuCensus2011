68a952b95a1effec331815d36f8f45c67b8cbe51
mike <mikefc@planckenergy.com>
2015-06-04 15:26:22 +1000

    R CMD CHECK fixes

--- R/package-creator.R
+++ R/package-creator.R
@@ -20,82 +20,83 @@ render_template <- function(data, template_file, output_file) {
     writeLines(whisker::whisker.render(template, data), output_file)
 }
 
-
-load("data/tableconfig.rda")
-load("data/geog.desc.rda")
-load("data/ABS.levels.rda")
-
-
-this_level      <- 'STE'
-this_level_desc <- (ABS.levels %>% filter(level == this_level))$desc
-package_path <- paste("../AuCensus2011", this_level, sep=".")
+create_level_package <- function() {
+    load("data/tableconfig.rda")
+    load("data/geog.desc.rda")
+    load("data/ABS.levels.rda")
 
 
-#-----------------------------------------------------------------------------
-# Copy across the package skeleton
-#-----------------------------------------------------------------------------
-# directory structure
-system(paste("cp -R 'package-skeleton/'", package_path))
-
-# project template
-command <- sprintf("cp 'package-templates/AuCensus2011.XXX.Rproj' %s/AuCensus2011.%s.Rproj",
-                   package_path,
-                   this_level)
-system(command)
-
-#-----------------------------------------------------------------------------
-# Render templated version of the README.md file.
-#-----------------------------------------------------------------------------
-render_template(data          =  list(level = this_level, level_desc = this_level_desc),
-                template_file = "package-templates/README.md",
-                output_file   = paste0(package_path, "/README.md"))
+    this_level      <- 'STE'
+    this_level_desc <- (ABS.levels %>% filter(level == this_level))$desc
+    package_path <- paste("../AuCensus2011", this_level, sep=".")
 
 
-#-----------------------------------------------------------------------------
-# Render templated version of the DESCRIPTION file.
-#-----------------------------------------------------------------------------
-render_template(data          =  list(level = this_level, level_desc = this_level_desc),
-                template_file = "package-templates/DESCRIPTION",
-                output_file   = paste0(package_path, "/DESCRIPTION"))
+    #-----------------------------------------------------------------------------
+    # Copy across the package skeleton
+    #-----------------------------------------------------------------------------
+    # directory structure
+    system(paste("cp -R 'package-skeleton/'", package_path))
+
+    # project template
+    command <- sprintf("cp 'package-templates/AuCensus2011.XXX.Rproj' %s/AuCensus2011.%s.Rproj",
+                       package_path,
+                       this_level)
+    system(command)
+
+    #-----------------------------------------------------------------------------
+    # Render templated version of the README.md file.
+    #-----------------------------------------------------------------------------
+    render_template(data          =  list(level = this_level, level_desc = this_level_desc),
+                    template_file = "package-templates/README.md",
+                    output_file   = paste0(package_path, "/README.md"))
 
 
-#-----------------------------------------------------------------------------
-# Save the region descriptions for just this level
-#-----------------------------------------------------------------------------
-region.description <- geog.desc %>% filter(level==this_level)
-data_path          <- paste0(package_path, "/data/region.description.rda")
-save(region.description, file=data_path)
-
-#-----------------------------------------------------------------------------
-# Render a templated version of the docs for this region.description
-#-----------------------------------------------------------------------------
-render_template(data          =  list(level = this_level, level_desc = this_level_desc),
-                template_file = "package-templates/region.description.R",
-                output_file   = paste0(package_path, "/R/region.description.R"))
+    #-----------------------------------------------------------------------------
+    # Render templated version of the DESCRIPTION file.
+    #-----------------------------------------------------------------------------
+    render_template(data          =  list(level = this_level, level_desc = this_level_desc),
+                    template_file = "package-templates/DESCRIPTION",
+                    output_file   = paste0(package_path, "/DESCRIPTION"))
 
 
-#-----------------------------------------------------------------------------
-# Load all ABS tables for this level, and save in long format to the
-# package directory
-#-----------------------------------------------------------------------------
-config <- tableconfig[['B01']]
-for (config in tableconfig) {
-    table  <- config$table
-    # Load the table into a named variable
-    assign(table, read_abs(profile='BCP', table=table, level=this_level, long=TRUE))
+    #-----------------------------------------------------------------------------
+    # Save the region descriptions for just this level
+    #-----------------------------------------------------------------------------
+    region.description <- geog.desc %>% filter(level==this_level)
+    data_path          <- paste0(package_path, "/data/region.description.rda")
+    save(region.description, file=data_path)
 
-    # Save the data to the given output directory
-    data_path    <- paste0(package_path, "/data/", table, ".rda")
-    save(list=c(table), file=data_path)
+    #-----------------------------------------------------------------------------
+    # Render a templated version of the docs for this region.description
+    #-----------------------------------------------------------------------------
+    render_template(data          =  list(level = this_level, level_desc = this_level_desc),
+                    template_file = "package-templates/region.description.R",
+                    output_file   = paste0(package_path, "/R/region.description.R"))
 
-    # Render a templated version of the docs for this file.
-    data          <- list(level=this_level, level_desc = this_level_desc, table=config$table, desc=config$desc)
-    template_file <- "package-templates/Bxx.R"
-    output_file   <- paste0(package_path, "/R/", table, ".R")
-    render_template(data, template_file, output_file)
+
+    #-----------------------------------------------------------------------------
+    # Load all ABS tables for this level, and save in long format to the
+    # package directory
+    #-----------------------------------------------------------------------------
+    config <- tableconfig[['B01']]
+    for (config in tableconfig) {
+        table  <- config$table
+        # Load the table into a named variable
+        assign(table, read_abs(profile='BCP', table=table, level=this_level, long=TRUE))
+
+        # Save the data to the given output directory
+        data_path    <- paste0(package_path, "/data/", table, ".rda")
+        save(list=c(table), file=data_path)
+
+        # Render a templated version of the docs for this file.
+        data          <- list(level=this_level, level_desc = this_level_desc, table=config$table, desc=config$desc)
+        template_file <- "package-templates/Bxx.R"
+        output_file   <- paste0(package_path, "/R/", table, ".R")
+        render_template(data, template_file, output_file)
+    }
+
+    # Generate the actual man pages from the roxygen comments in the R files
+    roxygen2::roxygenise(package_path)
+
+
 }
-
-# Generate the actual man pages from the roxygen comments in the R files
-roxygen2::roxygenise(package_path)
-
-
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AuCensus2011
 Tools for parsing 2011 Census DataPacks from the Australian Bureau of Statistics
+
+Install:
+```{r}
+devtools::install_github("coolbutuseless/AuCensus2011")
+```
